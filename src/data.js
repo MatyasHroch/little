672038223e3ab48c7f80109eed9309053acef8bc
MatyasHroch <matyas.hroch01@upol.cz>
2024-07-06 21:11:42 +0200

    One of the Last changes

--- src/data.js
+++ src/data.js
@@ -6,20 +6,26 @@ export default {
         description:
           "<p>Bohinjské jezero ([bohiňské], slovinsky Bohinjsko jezero, německy Wocheinersee) je průtočné podhorské jezero ve Slovinsku. Leží na severozápadě země mezi masivy Julských Alp, v nadmořské výšce 525 m. S rozlohou 318 ha jde o největší stálé jezero ve Slovinsku (Cerknické jezero má při maximálním naplnění podstatně větší plochu, ale jindy zase téměř vysychá). Je 4,35 km dlouhé, až 1,25 km široké, pobřeží má délku 10,9 km. Dosahuje maximální hloubky 45 m, přičemž čtyři pětiny jezera jsou hlubší než 20 m[1].</p><p>Jezero je ledovcovo-tektonického původu, prohlubeň byla zformována v poslední době ledové intenzivním erozním působením Bohinjského ledovce. Ve východní části jezera jsou pozůstatkem četné morény. Po ústupu ledovce se kotlina zaplnila vodou – původně byla hladina o 16 metrů výše než dnes a sahala až do dnešní Staré Fužiny.</p>",
         route: {
-          name: "cesta autem tam",
+          name: "Cesta autem na Bohinj",
           duration: "1 hod 30 min",
           distance: "80 km",
           map_src: "https://frame.mapy.cz/s/nehafojoku",
         },
         route_list: [
           {
-            name: "střední cesta",
+            name: "Krátká trasa k vodovádům",
+            duration: "1 hod 12 min",
+            distance: "2,2 km",
+            map_src: "https://frame.mapy.cz/s/ganajevudu",
+          },
+          {
+            name: "Zlatá střední cesta",
             duration: "4 hod 45 min",
             distance: "12,8 km",
             map_src: "https://frame.mapy.cz/s/dadoforafa",
           },
           {
-            name: "mírná trasa",
+            name: "Trasa pro náročné",
             duration: "6 hod 30 min",
             distance: "17 km",
             map_src: "https://frame.mapy.cz/s/gutonaduje",
